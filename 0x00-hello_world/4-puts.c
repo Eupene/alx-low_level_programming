1516500286e3a44b7966e98d56607b2e72053725
Eunice <eunicehogba@gmail.com>
2023-02-17 00:17:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - Entry Point
  *
  * Return: Always 0 (Success)
@@ -9,6 +9,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 
 }
