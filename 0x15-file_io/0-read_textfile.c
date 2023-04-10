e470a717cd81ff54067c76a7f26709ede639fdc0
EUNICE <eunicehogba@gmail.com>
2023-04-10 16:56:18 -0700

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints its contents
+ * read_textfile - reads a text file and prints it
  * @filename: the name of the file to be read
  * @letters: the number of letters to read and print
  *
@@ -9,37 +9,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, i, y;
+	int fd;
+	int i, y;
 	char *buf;
 
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
-
 	i = read(fd, buf, letters);
 	if (i < 0)
 	{
 		free(buf);
 		return (0);
 	}
-
 	buf[i] = '\0';
 	close(fd);
-
 	y = write(STDOUT_FILENO, buf, i);
 	if (y < 0)
 	{
 		free(buf);
 		return (0);
 	}
-
 	free(buf);
 	return (y);
 }
