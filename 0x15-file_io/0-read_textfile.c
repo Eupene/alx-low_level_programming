a6d5d90fcf18995a9b1efc822116f0bbc78e1818
EUNICE <eunicehogba@gmail.com>
2023-04-11 03:31:10 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,37 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to the name of the file to read
- * @letters: the number of letters to read and print
- *
- * Return: the actual number of letters that could be read and printed, or 0 if
- * the file could not be opened, read, or if filename is NULL
+ * read_textfile - a function that reads a
+ * text file and prints it to the POSIX standard output
+ * @filename: of the file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	ssize_t r, w;
-	if (!filename)
-		return (0);
-	
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	
-	r = read(fd, buf, letters);
-	if (r == -1)
+	register int _open, _read, _write;
+	register char *buffer = malloc(sizeof(char)  * letters);
+
+	if (!(buffer))
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	
-	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1 || w != r)
+	if (!(filename))
 	{
-		free(buf);
 		return (0);
 	}
-	free(buf);
-	return (w);
+	_open = open(filename, O_RDONLY);
+	_read = read(_open, buffer, letters);
+	_write = write(STDOUT_FILENO, buffer, _read);
+
+	if (_open == -1 || _read == -1 || _write == -1 || !(_write == _read))
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(_open);
+	return (_write);
 }
