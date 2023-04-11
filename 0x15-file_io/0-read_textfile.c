03683a09d658f8cb54d8811edabf3d9cc2f1468d
EUNICE <eunicehogba@gmail.com>
2023-04-11 03:20:48 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,42 @@
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the name of the file to read
+ * @letters: the number of letters to read and print
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: the actual number of letters that could be read and printed, or 0 if
+ * the file could not be opened, read, or if filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
-
-	if (filename == NULL)
+	int fd;
+	char *buf;
+	ssize_t r, w;
+	if (!filename)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	
+	r = read(fd, buf, letters);
+	if (r == -1)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-
-	free(buffer);
-	close(o);
-
+	
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || w != r)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
 	return (w);
 }
